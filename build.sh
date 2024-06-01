e45460eb4448cbd85ff0edef028d7f1185636201
bearben <genius-ben@qq.com>
2024-06-01 17:22:12 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,34 @@ if [ ! -d ${ADDR}/bin/ ]; then
 fi
 
 #ALC
-echo "Checking and compiling ALC"
+echo "Checking and compiling ALC ..."
 if [ ! -d ${ADDR}/ApproxLatCount/ ]; then
 	unzip ApproxLatCount.zip
 	cd ${ADDR}/ApproxLatCount
 	make
 	cd ../
 	mv ${ADDR}/ApproxLatCount/ApproxLatCount ${ADDR}/bin/.
+fi
+
+#z3
+echo "Checking and installing Z3 ..."
+if [ ! -f ${ADDR}/z3-master/lib/libz3.so ]; then
+	unzip z3-master.zip
+	cd ${ADDR}/z3-master
+	python scripts/mk_make.py --prefix=${ADDR}/z3-master
+	cd ${ADDR}/z3-master/build
+	make
+	make install
+fi
+
+#vinci
+echo "Cheking and compiling Vinci ..."
+if [ ! -d ${ADDR}/vinci-1.0.5/ ]; then
+	unzip vinci-1.0.5.zip
+	cd ${ADDR}/vinci-1.0.5
+	make
+	cd ../
+	mv ${ADDR}/vinci-1.0.5/vinci ${ADDR}/bin/vinci
 fi
 
 #volce main program
